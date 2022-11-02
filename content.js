ddae2b0ef3277f69be759e0a697698b2ea544b71
Kyle Lambert <kyle@calloutcreative.com>
2022-11-02 14:16:55 -0700

    Refactor font loading JS
    Fixes font loading issues.

--- content.js
+++ content.js
@@ -1,9 +1,8 @@
 /* This runs after a web page loads */
-function addStylesheetURL(url) {
-  var link = document.createElement('link');
-  link.rel = 'stylesheet';
-  link.href = url;
-  document.getElementsByTagName('head')[0].appendChild(link);
-}
-  
-addStylesheetURL("https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@400;600;700&family=Press+Start+2P&display=swap");
+var getHeader = document.getElementsByTagName('head').item(0);
+var linkStyle = document.createElement("link");
+var url = "https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@400;600;700&family=Press+Start+2P&display=swap";
+linkStyle.href = url;
+linkStyle.rel = 'stylesheet';
+linkStyle.type = "text/css"; 
+getHeader.appendChild(linkStyle);
